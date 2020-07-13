9a1d0728812758d3cf2801a623bb809a3542b5b2
渡部吉平 <kippeiwatanabe@MacBook-Pro.local>
2020-07-13 22:37:07 +0900

    add css file

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request, nickname, age):
-    result = "your account: '" + nickname + "' (" + str(age) + ")."
-    return HttpResponse(result)
+def index(request):
+    params = {
+        "title":"TODO/Index",
+        "msg":"This is a todo application.",
+        "goto":"next",
+    }
+    return render(request, "todo/index.html", params)
+
+def next(request):
+    params = {
+        "title":"TODO/Next",
+        "msg":"This is another page.",
+        "goto":"index",
+    }
+    return render(request, "todo/index.html", params)
+
 
